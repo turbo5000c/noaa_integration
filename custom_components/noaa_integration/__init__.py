67aa5cec688ec941e26960778349272dcdee6e2c
turbo5000c <turbo5000c@hotmail.com>
2024-09-12 21:30:04 -0400

    Update __init__.py

--- custom_components/noaa_integration/__init__.py
+++ custom_components/noaa_integration/__init__.py
@@ -8,8 +8,8 @@ _LOGGER = logging.getLogger(__name__)
 DOMAIN = "noaa_integration"
 
 def setup(hass, config):
-    """Set up the MyWeather component."""
-    _LOGGER.info("Setting up MyWeather integration")
+    """Set up the NOAA component."""
+    _LOGGER.info("Setting up NOAA integration")
 
     # Load the platform for sensor
     discovery.load_platform(hass, 'sensor', DOMAIN, {}, config)
