adefba3a4f052356a9353ca0a564adc6bf612c19
turbo5000c <turbo5000c@hotmail.com>
2024-09-13 00:22:14 +0000

    test

--- custom_components/noaa_integration/__init__.py
+++ custom_components/noaa_integration/__init__.py
@@ -1,2 +1,17 @@
+# __init__.py
+import logging
+
+from homeassistant.helpers import discovery
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "noaa_integration"
+
 def setup(hass, config):
+    """Set up the MyWeather component."""
+    _LOGGER.info("Setting up MyWeather integration")
+
+    # Load the platform for sensor
+    discovery.load_platform(hass, 'sensor', DOMAIN, {}, config)
+
     return True
--- custom_components/noaa_integration/sensor.py
+++ custom_components/noaa_integration/sensor.py
@@ -1,21 +1,32 @@
-from homeassistant.components.sensor import SensorEntity
+# sensor.py
+import requests
+from homeassistant.helpers.entity import Entity
 
-class NOAAKpIndexSensor(SensorEntity):
-    def __init__(self, name):
-        self._name = name
+DOMAIN = 'noaa_integration'
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the MyWeather sensor."""
+    add_entities([MyWeatherSensor()])
+
+class MyWeatherSensor(Entity):
+    """Representation of a MyWeather sensor."""
+
+    def __init__(self):
+        """Initialize the sensor."""
         self._state = None
 
     @property
     def name(self):
-        return self._name
+        """Return the name of the sensor."""
+        return 'My Weather Temperature'
 
     @property
     def state(self):
+        """Return the state of the sensor."""
         return self._state
 
     def update(self):
-        # Set a static value for testing purposes
-        self._state = 5
-
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    add_entities([NOAAKpIndexSensor("NOAA Kp Index")])
+        """Fetch new state data for the sensor."""
+        response = requests.get('https://api.weatherapi.com/v1/current.json?key=your_api_key&q=London')
+        data = response.json()
+        self._state = data['current']['temp_c']
