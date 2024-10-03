e05f48edbd0c710eb9a0983f06223c435f8c5182
turbo5000c <turbo5000c@hotmail.com>
2024-10-02 21:30:40 -0400

    Update sensor.py

--- custom_components/noaa_integration/sensor.py
+++ custom_components/noaa_integration/sensor.py
@@ -37,6 +37,7 @@ class PlanetaryKIndexSensor(Entity):
     def __init__(self):
         """Initialize the Planetary K-index sensor."""
         self._state = None
+        self._rating = None  # To store the rating based on the K-index
 
     @property
     def name(self):
@@ -48,6 +49,18 @@ class PlanetaryKIndexSensor(Entity):
         """Return the state of the sensor."""
         return self._state
 
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'NOAA Planetary K-index Rating'
+
+    @property
+    def extra_state_attributes(self):
+        """Return additional state attributes, such as the rating."""
+        return {
+            'rating': self._rating  # Include rating as an additional attribute
+        }
+
     def update(self):
         """Fetch new state data for the K-index."""
         response = requests.get('https://services.swpc.noaa.gov/json/planetary_k_index_1m.json')
@@ -55,3 +68,13 @@ class PlanetaryKIndexSensor(Entity):
             data = response.json()
             self._state = data[-1].get('kp_index', 'unknown')
 
+            # Determine the rating based on the K-index value
+            if self._state != 'unknown':  # Ensure valid state
+                if self._state < 2:
+                    self._rating = 'low'
+                elif 2 <= self._state < 5:
+                    self._rating = 'moderate'
+                else:
+                    self._rating = 'high'
+            else:
+                self._rating = 'unknown'
