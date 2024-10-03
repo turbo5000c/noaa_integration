a36482620f901330ffb74b02fc9b2fc8038ae191
turbo5000c <turbo5000c@hotmail.com>
2024-10-02 23:47:14 -0400

    Update image.py

--- custom_components/noaa_integration/image.py
+++ custom_components/noaa_integration/image.py
@@ -1,35 +1,33 @@
 import requests
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import Image
 
 DOMAIN = 'noaa_integration'
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the Geoelectric Field Image sensor."""
-    geoelectric_image_sensor = GeoelectricFieldImageSensor()
+    """Set up the Geoelectric Field Image entity."""
+    geoelectric_image_entity = GeoelectricFieldImage()
 
-    # Add the geoelectric image sensor entity
-    add_entities([geoelectric_image_sensor])
+    # Add the geoelectric image entity
+    add_entities([geoelectric_image_entity])
 
-class GeoelectricFieldImageSensor(Entity):
-    """Representation of the Geoelectric Field Image Sensor."""
+class GeoelectricFieldImage(Image):
+    """Representation of the Geoelectric Field Image."""
 
     def __init__(self):
-        """Initialize the Geoelectric Field Image sensor."""
+        """Initialize the image entity."""
+        super().__init__()
         self._image_url = None
 
     @property
     def name(self):
-        """Return the name of the sensor."""
+        """Return the name of the entity."""
         return 'Geoelectric Field Image'
 
     @property
-    def entity_picture(self):
-        """Return the URL of the latest image."""
+    def image_url(self):
+        """Return the URL of the latest geoelectric field image."""
         return self._image_url
 
     def update(self):
         """Fetch the latest geoelectric field image."""
-        image_url = 'https://services.swpc.noaa.gov/images/animations/geoelectric/InterMagEarthScope/EmapGraphics_1m/latest.png'
-        response = requests.get(image_url)
-        if response.status_code == 200:
-            self._image_url = image_url
+        self._image_url = 'https://services.swpc.noaa.gov/images/animations/geoelectric/InterMagEarthScope/EmapGraphics_1m/latest.png'
